c196b7da93e7eb535d286a280c7fb41cf01209e9
Pedro Carvalho Brom <pcbrom@gmail.com>
2016-03-09 09:49:34 -0300

    Update avaliacao_1._131028100020.R

--- Avaliacoes/avaliacao_1._131028100020.R
+++ Avaliacoes/avaliacao_1._131028100020.R
@@ -1,21 +1,21 @@
 # ======================================================================
 
-# CURSO: INTRODUÇÃO À PROGRAMAÇÃO COM R.
-# AVALIAÇÃO: 01.
+# CURSO: INTRODUÃ‡ÃƒO Ã€ PROGRAMAÃ‡ÃƒO COM R.
+# AVALIAÃ‡ÃƒO: 01.
 # URL: goo.gl/Xsnujp
 
 # ======================================================================
 
 # PROBLEMA PROPOSTO:
 
-# Esta é a equacao de Torricelli V^2 = U^2 + 2 * a * D, 
-# onde V é a velocidade, U é a velocidade inicial, a é a aceleracao, 
-# D a distância percorrida, * significa multiplicação e ^ signifca 
+# Esta Ã© a equacao de Torricelli V^2 = U^2 + 2 * a * D, 
+# onde V Ã© a velocidade, U Ã© a velocidade inicial, a Ã© a aceleracao, 
+# D a distÃ¢ncia percorrida, * significa multiplicaÃ§Ã£o e ^ signifca 
 # elevado a.
 
-# Faça um programa que leia os valores de U, a e D (nessa ordem) 
-# e escreva o valor da variável resp = V - U, com 
-# precisão de duas casas decimais. 
+# FaÃ§a um programa que leia os valores de U, a e D (nessa ordem) 
+# e escreva o valor da variÃ¡vel resp = V - U, com 
+# precisÃ£o de duas casas decimais. 
 
 # ======================================================================
 
@@ -25,14 +25,14 @@
 
 # 2. Seu programa a ser enviado para:
 #     github.com/pcbrom/IPR/tree/master/Avaliacoes
-#   E deve ser salvo com a seguinte máscara:
-#     avaliacao_[0-7]_[numero de matrícula/SIAPE].R
+#   E deve ser salvo com a seguinte mÃ¡scara:
+#     avaliacao_[0-7]_[numero de matrÃ­cula/SIAPE].R
 
 # 3. Para formatar o numero de casas decimais use o comando round(x,d), 
-#     onde 'x' é o objeto e 'd' é o numero de casas decimais desejado.
+#     onde 'x' Ã© o objeto e 'd' Ã© o numero de casas decimais desejado.
 #     Exemplo:
 #     - Entrada: round(sqrt(2), 3)
-#     - Saída: 1.414
+#     - SaÃ­da: 1.414
 
 # ======================================================================
 
@@ -42,22 +42,22 @@
 # Saida: 2.00
 
 # =====================================================================
-# A RESOLUÇÃO DEVE SER FEITA ABAIXO DA LINHA 47.
+# A RESOLUÃ‡ÃƒO DEVE SER FEITA ABAIXO DA LINHA 47.
 
 # ======================================================================
 
-u=5
-a=2
-d=6
-v=((u^2)+2*a*d)
+u=5 # (01 ponto - problema: deveria ser letra maiúscula a entrada)
+a=2 # (02 pontos)
+d=6 # (01 ponto - problema: deveria ser letra maiúscula a entrada)
+v=((u^2)+2*a*d) 
 operacao.1=round(sqrt(v),2)
 print(V)
 k=((v^2)-2*a*d)
 operacao.2=round(sqrt(k),2)
-resp=round((v-k),2)
+resp=round((v-k),2) # (02 pontos - Arredondamento ok, mas com objeto errado)
 
 
-N�O CONSEGUIR CHEGAR NA SAIDA OFICIAL DE 2.
+NÃO CONSEGUIR CHEGAR NA SAIDA OFICIAL DE 2.
 
-
+# Nota final: 06 pontos.
 
