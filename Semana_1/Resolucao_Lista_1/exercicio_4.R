d6c6ccc5f8efd7a81c2bbc6659519fd028a0886f
miltonjuliano <miltonjuliano@gmail.com>
2016-03-10 23:33:31 -0300

    Update exercicio_4.R

--- Semana_1/Resolucao_Lista_1/exercicio_4.R
+++ Semana_1/Resolucao_Lista_1/exercicio_4.R
@@ -4,5 +4,5 @@ a=(n)+(n+1)+(n+2)+(n+3)+(n+4)
 a/5
 
 #outra forma de fazer
-a=c(25,54,32,29,12)
-mean(a)
+a=c(25,54,32,29,12) # o "c" cria um conjunto ou lista.
+mean(a) #tira a media do conjunto indicado.
