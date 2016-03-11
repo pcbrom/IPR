110dca04e900a220d77137b82617da2998495f33
miltonjuliano <miltonjuliano@gmail.com>
2016-03-10 22:29:57 -0300

    Update exercicio_4.R

--- Semana_2/Resolucao_Lista_2/exercicio_4.R
+++ Semana_2/Resolucao_Lista_2/exercicio_4.R
@@ -1,1 +1,12 @@
-
+#milton
+qc = 4  #quantidade de cadeiras
+qm = 2   #quantidade de mesas
+qb =  7   #quantidade de bancos
+c = 50 #cadeiras
+if ( qc > 3){ c = 30}
+m = 150 #mesas
+if (qm > 4) {m = 120}
+b = 10   #bancos
+if (qb > 5) {b = 8}
+resp = (qc*c) + (qm*m) + (qb*b)
+print(resp)
