b7a2961ab3ee4541d49b9e5c7c78b9d2971c4399
miltonjuliano <miltonjuliano@gmail.com>
2016-03-10 22:27:48 -0300

    Update exercicio_3.R

--- Semana_2/Resolucao_Lista_2/exercicio_3.R
+++ Semana_2/Resolucao_Lista_2/exercicio_3.R
@@ -1,1 +1,9 @@
-
+#milton
+y=15
+if(is.character(y)){
+  if(y=="a"||y=="e"||y=="i"||y=="o"||y=="u"){
+    "È uma vogal"
+  }else {"É um caracter Comum"}
+}else if(is.numeric(y)){
+  "É um algarismo"
+}
