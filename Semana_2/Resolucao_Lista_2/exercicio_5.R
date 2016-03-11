9afa25d3df8900b7511ff35d4ba88e511f7f6019
miltonjuliano <miltonjuliano@gmail.com>
2016-03-10 22:30:24 -0300

    Update exercicio_5.R

--- Semana_2/Resolucao_Lista_2/exercicio_5.R
+++ Semana_2/Resolucao_Lista_2/exercicio_5.R
@@ -1,1 +1,10 @@
-
+#milton
+pg = 500
+d = 2
+if(d>=26 && d<=31) {pg = (4*pg/100)+pg}
+if(d>=21 && d<=25) {pg = (2*pg/100)+pg}
+if(d>=16 && d<=20) {pg = pg}
+if(d>=11 && d<=15) {pg = pg - (2*pg/100)}
+if(d>=6 && d<=10) {pg = pg - (4*pg/100)}
+if(d>=1 && d<=5) {pg = pg - (6*pg/100)}
+print(pg)
