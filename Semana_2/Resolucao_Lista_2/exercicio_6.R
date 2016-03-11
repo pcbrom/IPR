c883f34567cfb9c8a7d5cefcdcaf55108f6a6554
miltonjuliano <miltonjuliano@gmail.com>
2016-03-10 22:32:15 -0300

    Update exercicio_6.R

--- Semana_2/Resolucao_Lista_2/exercicio_6.R
+++ Semana_2/Resolucao_Lista_2/exercicio_6.R
@@ -1,1 +1,8 @@
-
+#milton
+vc = 30 #valor de compra
+vv = 0   #valor de venda
+if(vc<10) {vv = vc+(70*vc/100)}
+if(vc>=10 && vc<30) {vv= vc+(50*vc/100)}
+if(vc>=30 && vc < 100) {vv = vc+(40*vc/100)}
+if(vc>=100) {vv = vc+(30*vc/100)}
+print(vv)
