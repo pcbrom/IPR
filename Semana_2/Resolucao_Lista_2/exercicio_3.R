3a75003c03f5ed46486b72071a56b0e100d88f93
miltonjuliano <miltonjuliano@gmail.com>
2016-03-15 17:05:43 -0300

    Update exercicio_3.R

--- Semana_2/Resolucao_Lista_2/exercicio_3.R
+++ Semana_2/Resolucao_Lista_2/exercicio_3.R
@@ -1,7 +1,7 @@
 #milton
 y = 15
 if(is.character(y)){
-  if(y=="a"||y=="e"||y=="i"||y=="o"||y=="u"|| y=="A"||y=="B"||y=="C"||y=="D"||y=="E"){
+  if(y=="a"||y=="e"||y=="i"||y=="o"||y=="u"|| y=="A"||y=="E"||y=="I"||y=="O"||y=="U"){
         "È uma vogal"
       }else {"É um caracter Comum"}
   }else {
