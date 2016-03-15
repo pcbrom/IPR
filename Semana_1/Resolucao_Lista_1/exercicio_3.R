54832c8ac520f3f91aed46d1413454460f4932b9
miltonjuliano <miltonjuliano@gmail.com>
2016-03-15 16:28:51 -0300

    Update exercicio_3.R

--- Semana_1/Resolucao_Lista_1/exercicio_3.R
+++ Semana_1/Resolucao_Lista_1/exercicio_3.R
@@ -1,6 +1,14 @@
-#milton
+#milton (enjambration)
 pop = 2000000
 a = (2*(pop)/100)+(pop)
 b = a +(2*(pop)/100)
 c = b +(2*(pop)/100)
 print(c)
+
+# melhoramentos
+
+pop.i = 200000
+tx = 0,05
+n = 3 
+pop= pop.i+ (1+tx)^n
+print (pop)
