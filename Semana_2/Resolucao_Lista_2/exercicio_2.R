11b1134ea2b0ada2823b1d2cdbc6298310e166c2
miltonjuliano <miltonjuliano@gmail.com>
2016-03-15 16:36:00 -0300

    Update exercicio_2.R

--- Semana_2/Resolucao_Lista_2/exercicio_2.R
+++ Semana_2/Resolucao_Lista_2/exercicio_2.R
@@ -1,7 +1,10 @@
 #milton
-vp = 150 #valor do produto
-d = 9 #data da compra
-
-  if(d<10) {vp-(20*vp/100)
-    } else{vp}
-
+vp = abs(150) #valor do produto
+d = abs(9) #data da compra
+if(d==0) {
+  "seu mané, data inexistente"
+} else if (d<10) {
+  vp-(20*vp/100)
+} else {
+  vp
+}
