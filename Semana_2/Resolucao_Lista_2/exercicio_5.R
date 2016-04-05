52d41a413cd1ffbc52d8a38df2fd2d94c0919b4a
Luiz Fernando Viana <lfcviana@outlook.com>
2016-04-05 16:51:05 -0300

    Update exercicio_5.R

--- Semana_2/Resolucao_Lista_2/exercicio_5.R
+++ Semana_2/Resolucao_Lista_2/exercicio_5.R
@@ -8,3 +8,39 @@ if(d>=11 && d<=15) {pg = pg - (2*pg/100)}
 if(d>=6 && d<=10) {pg = pg - (4*pg/100)}
 if(d>=1 && d<=5) {pg = pg - (6*pg/100)}
 print(pg)
+
+
+#FERNANDO
+
+d=31 #dia do pagamento
+v=100 #valor real
+
+if(d>=1 && d<=5){
+  
+  print(v*0.94)
+  
+  }else if(d>=6 && d<=10){
+  
+    print(v*0.96)
+    
+    }else if(d>=11 && d<=15){
+        
+      print(v*0.98)
+      
+      }else if(d>=16 && d<=20){
+        
+        print(v)
+        
+        }else if(d>=21 && d<=25){
+          
+          print(v*1.02)
+          
+          }else if(d>=26 && d<=31){
+            
+            print(v*1.04)
+          
+          }else{
+            
+            print("Dia inexistente")
+            
+          }
