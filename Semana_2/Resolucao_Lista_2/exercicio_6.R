ed85ca3ec277034789ead54d9055445e6eb1db4b
Luiz Fernando Viana <lfcviana@outlook.com>
2016-04-05 17:06:34 -0300

    Update exercicio_6.R

--- Semana_2/Resolucao_Lista_2/exercicio_6.R
+++ Semana_2/Resolucao_Lista_2/exercicio_6.R
@@ -6,3 +6,25 @@ if(vc>=10 && vc<30) {vv= vc+(50*vc/100)}
 if(vc>=30 && vc < 100) {vv = vc+(40*vc/100)}
 if(vc>=100) {vv = vc+(30*vc/100)}
 print(vv)
+
+#FERNANDO
+
+compra=35
+
+venda=if(compra<10){
+  
+  print(compra*1.7)
+  
+  }else if(compra>=10 && compra<30){
+   
+    print(compra*1.5)
+    
+    }else if(compra>=30 && compra<100){
+    
+      print(compra*1.4)
+      
+      }else if(compra>=100){
+        
+        print(compra*1.3)
+
+        }
