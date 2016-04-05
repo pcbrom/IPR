473f4021bb0d124e963c8121e378708ba67f4771
Luiz Fernando Viana <lfcviana@outlook.com>
2016-04-05 18:09:57 -0300

    Update exercicio_7.R

--- Semana_2/Resolucao_Lista_2/exercicio_7.R
+++ Semana_2/Resolucao_Lista_2/exercicio_7.R
@@ -3,3 +3,23 @@ al = 1.75    #altura
 sx = "m"      #sexo
 if(sx== "m") {(72.7*al)-58
   }else {(62.1*al)-44.7}
+
+#FERNANDO
+
+a = 1.65 #altura
+
+s <- scan(n=1, what=character())
+
+aideal = if(s == "M"){
+  
+  print((72.7 * a) - 58)
+
+  }else if(s == "F"){
+  
+    print((62.1 * a) - 44.7)
+
+    }else{
+      
+      print("sexo não existente")
+      
+    }
