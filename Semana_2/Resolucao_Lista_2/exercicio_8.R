d90abfba884d6b8f41f466d3a6143f399ca75c78
Luiz Fernando Viana <lfcviana@outlook.com>
2016-04-05 21:23:22 -0300

    Update exercicio_8.R

--- Semana_2/Resolucao_Lista_2/exercicio_8.R
+++ Semana_2/Resolucao_Lista_2/exercicio_8.R
@@ -2,3 +2,9 @@
 a = c(1,2,52,4) # o "c" cria um conjunto/lista. 
 max(a) #indica o maior elemento do conjunto/listalista
 min(a) #indica o menor elemento do conjunto/listalista
+
+#FERNANDO
+
+x <- scan(n=4)
+max(x)
+min(x)
